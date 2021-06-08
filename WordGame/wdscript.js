c771f8d177dccf4ab04331dce56d2ad486330a54
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-06-08 10:41:52 -0400

    remove accent words, limit word gen to 10 words

--- WordGame/wdscript.js
+++ WordGame/wdscript.js
@@ -1,5 +1,18 @@
+// function randomWord(array) {
+//   let genWord = array[Math.floor(Math.random() * array.length)];
+//   if (genWord.length < 8 && genWord.length > 2) {
+//     return genWord;
+//   } else {
+//     randomWord(array);
+//   }
+// }
+//Limit it to words with 8 or less and more than 2 letters.
 function randomWord(array) {
-  return array[Math.floor(Math.random() * array.length)];
+  let genWord = array[Math.floor(Math.random() * array.length)];
+  while (genWord.length > 8 || genWord.length <= 2) {
+    genWord = array[Math.floor(Math.random() * array.length)];
+  }
+  return genWord;
 }
 
 function createNewWord(array) {
@@ -8,7 +21,13 @@ function createNewWord(array) {
   return newWord;
 }
 
+let limit = 10;
 function addWord(array) {
-  var prepWord = createNewWord(array);
-  document.getElementById("bigBox").appendChild(prepWord);
+  if (limit > 0) {
+    var prepWord = createNewWord(array);
+    document.getElementById("bigBox").appendChild(prepWord);
+    limit -= 1;
+  } else {
+    console.log("No words remain");
+  }
 }
