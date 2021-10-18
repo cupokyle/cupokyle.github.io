46af0fdd5beb7c23f08e8fd30425378ffe822476
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-18 11:31:05 -0400

    Begin custom resizing of project components

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "project.css";
+import "./project.css";
 
 export default function Project({
   project: { id, title, projectUrl, projectImgUrl, state },
@@ -8,38 +8,39 @@ export default function Project({
   onPinProject,
 }) {
   return (
-    <div className={`list-item ${state}`}>
-      <label className="checkbox">
-        <input
-          type="checkbox"
-          defaultChecked={state === "PROJECT_ARCHIVED"}
-          disabled={true}
-          name="checked"
-        />
-        <span
-          className="checkbox-custom"
-          onClick={() => onArchiveProject(id)}
-        />
-      </label>
+    <div className={`listItem ${state}`}>
       <div className="title">
+        <label className="checkbox">
+          <input
+            type="checkbox"
+            defaultChecked={state === "PROJECT_ARCHIVED"}
+            disabled={true}
+            name="checked"
+          />
+          <span
+            className="checkbox-custom"
+            onClick={() => onArchiveProject(id)}
+          />
+        </label>
         <input
           type="text"
           value={title}
           readOnly={true}
           placeholder="Input title"
         />
+        <div className="actions" onClick={(event) => event.stopPropagation()}>
+          {state !== "PROJECT_ARCHIVED" && (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a onClick={() => onPinProject(id)}>
+              <span className={`icon-star`} />
+            </a>
+          )}
+        </div>
       </div>
-      <a className="project-image" href={projectUrl}>
-        <img src={projectImgUrl} alt="project thumbnail" />
-      </a>
-
-      <div className="actions" onClick={(event) => event.stopPropagation()}>
-        {state !== "PROJECT_ARCHIVED" && (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <a onClick={() => onPinProject(id)}>
-            <span className={`icon-star`} />
-          </a>
-        )}
+      <div className="projectImage">
+        <a href={projectUrl}>
+          <img src={projectImgUrl} alt="project thumbnail" />
+        </a>
       </div>
     </div>
   );
--- src/components/Project.stories.jsx
+++ src/components/Project.stories.jsx
@@ -16,7 +16,7 @@ Default.args = {
     title: "Test Project",
     projectUrl: "https://www.lego.com",
     projectImgUrl:
-      "https://img.buzzfeed.com/buzzfeed-static/static/2017-03/23/17/tmp/buzzfeed-prod-fastlane-01/tmp-name-2-1059-1490303712-5_dblbig.jpg",
+      "https://cdn.vox-cdn.com/thumbor/gsgRcVw25MIEzXnI0hNuXUP-ZJE=/0x39:650x472/920x613/filters:focal(0x39:650x472):format(webp)/cdn.vox-cdn.com/assets/960660/Screen_Shot_2012-02-20_at_11.36.09_AM.png",
     state: "PROJECT_INBOX",
     updatedAt: new Date(2021, 0, 1, 9, 0),
   },
