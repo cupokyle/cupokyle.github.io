25873bea8d1d71a4d88d3b50c01b3ed8640a39d6
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-22 13:15:23 -0400

    Just playing around with gifs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,5 +3,9 @@ import PropTypes from "prop-types";
 import "../App.css";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>FUCK!</h1>
+    </main>
+  );
 }
