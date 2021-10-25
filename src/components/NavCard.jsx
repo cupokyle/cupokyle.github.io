e6757687db9ec5922f86f392703e42702d41a4b2
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-25 14:50:53 -0400

    small tweaks

--- src/components/NavCard.jsx
+++ src/components/NavCard.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./nav.css";
 
 export default function NavCard({ title, navItemImgUrl, itemUrl }) {
-  return <div className="navCard">{title}</div>;
+  return <div className="navCard"></div>;
 }
